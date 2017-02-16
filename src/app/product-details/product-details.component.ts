4d1ef8e3a6703a9ebde6dfe99ef82d50f63ccfb3
Shubhankar Dandekar <shubhankard34@gmail.com>
2017-02-16 14:57:32 +0530

    SG-6: Back Button

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,5 +1,8 @@
 import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
+
 import { Product } from '../interfaces';
+import { TabChange } from './../util/tab-change.util';
 
 @Component({
   selector: 'app-product-details',
@@ -9,4 +12,11 @@ import { Product } from '../interfaces';
 export class ProductDetailsComponent {
     @Input()
     public product: Product;
+
+    constructor(private router: Router, private tabChange: TabChange) {}
+
+    public backToProducts(): void {
+      this.router.navigate(['app-main-page']);
+      this.tabChange.selectedMainTabIndex = 1;
+    }
 }
